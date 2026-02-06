6ee41fce1fdaab59519b5064ae1061bfcae1f126
Comsterkar4 <nlvietkar4@gmail.com>
2026-02-06 23:34:56 +0700

    add delete

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -8,18 +8,17 @@ interface Props {
   onDelete: (id: string) => void;
 }
 
-// 1. THÊM onDelete VÀO ĐÂY
 export const TodoItem: React.FC<Props> = ({ item, onToggle, onDelete }) => {
   return (
     <View style={styles.itemTodo}>
-      {/* Checkbox */}
+  
       <TouchableOpacity onPress={() => onToggle(item.id)}>
         <Text style={styles.icon}>
           {item.isCompleted ? '✅' : '⬜'}
         </Text>
       </TouchableOpacity>
 
-      {/* Nội dung (Title + Description) */}
+    
       <View style={styles.contentContainer}>
         <Text style={[styles.title, item.isCompleted ? styles.done : styles.notDone]}>
           {item.title}
@@ -29,7 +28,7 @@ export const TodoItem: React.FC<Props> = ({ item, onToggle, onDelete }) => {
         )}
       </View>
 
-      {/* 2. ĐƯA NÚT XÓA RA NGOÀI (Nằm ngang hàng với contentContainer) */}
+      
       <TouchableOpacity 
         style={styles.deleteButton} 
         onPress={() => onDelete(item.id)}
@@ -56,15 +55,15 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 2,
     shadowOffset: { width: 0, height: 1 },
-    alignItems: 'center', // Căn giữa theo trục dọc để nút xóa không bị lệch
+    alignItems: 'center', 
   },
   icon: {
     fontSize: 20,
     marginRight: 10,
-    // marginTop: 2, // Bỏ cái này đi để căn giữa chuẩn hơn
+  
   },
   contentContainer: {
-    flex: 1, // Chiếm hết khoảng trống để đẩy nút Xóa về sát lề phải
+    flex: 1, 
   },
   title: {
     fontSize: 16,
@@ -86,11 +85,11 @@ const styles = StyleSheet.create({
 
 
   deleteButton: {
-    backgroundColor: '#ffdddd', // Nền đỏ nhạt
+    backgroundColor: '#ffdddd',
     paddingVertical: 6,
     paddingHorizontal: 10,
     borderRadius: 4,
-    marginLeft: 10, // Cách nội dung một đoạn
+    marginLeft: 10, 
   },
   deleteText: {
     color: 'red',
